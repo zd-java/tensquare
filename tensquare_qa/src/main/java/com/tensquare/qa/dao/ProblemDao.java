a11c044e3b8220f488ef83c51000d8883502ea7d
zd-demo <2304138065@qq.com>
2020-03-06 10:35:06 +0800

    add 问答微服务 tensquare_qa

---
+++ tensquare_qa/src/main/java/com/tensquare/qa/dao/ProblemDao.java
@@ -0,0 +1,25 @@
+package com.tensquare.qa.dao;
+
+import com.tensquare.qa.pojo.Problem;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * 数据访问接口
+ *
+ * @author Administrator
+ */
+public interface ProblemDao extends JpaRepository<Problem, String>, JpaSpecificationExecutor<Problem> {
+
+    @Query(value = "select * from tb_problem,tb_pl where id = problemid and labelid = ? order by replytime desc", nativeQuery = true)
+    public Page<Problem> newList(String labelid, Pageable pageable);
+
+    @Query(value = "select * from tb_problem,tb_pl where id = problemid and labelid = ? order by reply desc", nativeQuery = true)
+    public Page<Problem> hotList(String labelid, Pageable pageable);
+
+    @Query(value = "select * from tb_problem,tb_pl where id = problemid and labelid = ? and reply = 0 order by createtime desc", nativeQuery = true)
+    public Page<Problem> waitList(String labelid, Pageable pageable);
+}
